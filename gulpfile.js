9a6d15f37958ff751ae03ba879f33412241ad6da
stefan <sopic.stefan@gmail.com>
2015-09-26 00:05:22 +0200

    Input and submit plus working gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
 
-gulp.task('default', function() {
-  console.log('********************');
-  // place code for your default task here
+gulp.task('default', ['copy-index-html', 'build-less']);
+
+//1. Copy index.html from src to bin
+gulp.task('copy-index-html', function() {
+  gulp.src('src/index.html')
+    .pipe(gulp.dest('bin/'));
 });
+
+//2. Compile main.less in bin/css
+gulp.task('build-less', function(){
+  return gulp.src('src/assets/less/main.less')
+    .pipe(less())
+    .pipe(gulp.dest('bin/css/'));
+});
+
+
+//3. React to changes in files via watch
