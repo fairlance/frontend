403d715e8ef6922446ba7226ecd452ebb47de1a7
stefan <sopic.stefan@gmail.com>
2015-09-26 22:42:37 +0200

    #FRT-10 error handling

--- src/app/landing/landing.js
+++ src/app/landing/landing.js
@@ -1,22 +1,30 @@
 $(document).ready(function () {
+  var input = $('input');
   var submitEmail = function () {
     var obj = {
-      'email': $('input').val()
+      'email': input.val()
     };
     var urlElements = window.location.href.split('/');
     var url = urlElements[0] + '//' + urlElements[2].split(':')[0] + ':3000/register';
-    $.post(url, obj, function (data) {
-      console.log(data);
-    });
+    $.post(url, obj)
+      .done(function () {
+        input.attr('placeholder', 'Thank you!');
+        input.removeClass('error');
+      })
+      .fail(function (data) {
+        console.log(data);
+        input.addClass('error');
+        input.attr('placeholder', data.responseJSON.error);
+      });
   };
   $('.btn').click(function () {
     submitEmail();
-    $('input').val('');
+    input.val('');
   });
-  $('input').keypress(function (e) {
+  input.keypress(function (e) {
     if (e.which == 13) {
       submitEmail();
-      $('input').val('');
+      input.val('');
       return false;
     }
   });
