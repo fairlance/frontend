3aaffd0d762f1cfe89c5492d8902b4c9aea2beac
stefan <sopic.stefan@gmail.com>
2015-09-26 22:09:05 +0200

    #FRT-10 this js should use url no matter if it has port or not

--- src/app/landing/landing.js
+++ src/app/landing/landing.js
@@ -1,21 +1,22 @@
-
 $(document).ready(function () {
   var submitEmail = function () {
     var obj = {
       'email': $('input').val()
     };
-    var url = window.location.href;
-    url = url.split('/')[0] + ':3000/register';
+    var urlElements = window.location.href.split('/');
+    var url = urlElements[0] + '//' + urlElements[2].split(':')[0] + ':3000/register';
     $.post(url, obj, function (data) {
       console.log(data);
     });
   };
   $('.btn').click(function () {
-   submitEmail();
+    submitEmail();
+    $('input').val('');
   });
   $('input').keypress(function (e) {
     if (e.which == 13) {
       submitEmail();
+      $('input').val('');
       return false;
     }
   });
