3a11a1430c8a209204a8a733635a7458d6bf2bef
stefan <sopic.stefan@gmail.com>
2015-09-26 15:57:41 +0200

    #FRT-10 Markup for landing page done.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var watch = require('gulp-watch');
 
 gulp.task('default', ['copy-index-html', 'build-less']);
 
 //1. Copy index.html from src to bin
-gulp.task('copy-index-html', function() {
+gulp.task('copy-html', function() {
   gulp.src('src/index.html')
     .pipe(gulp.dest('bin/'));
+});
+
+gulp.task('copy-files', function() {
+  gulp.src('src/assets/images/**/*')
+    .pipe(gulp.dest('bin/assets/images'));
 });
 
 //2. Compile main.less in bin/css
@@ -16,5 +22,8 @@ gulp.task('build-less', function(){
     .pipe(gulp.dest('bin/css/'));
 });
 
-
 //3. React to changes in files via watch
+gulp.task('watch', function () {
+  gulp.watch('src/**/*.less', ['build-less', 'copy-files']);
+  gulp.watch('src/**/*.html', ['copy-html', 'copy-files']);
+});
