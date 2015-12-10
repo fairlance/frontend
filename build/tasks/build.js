e5dad58d62e16268b13e9c633fe80b3339daeba0
stefan <sopic.stefan@gmail.com>
2015-12-10 21:07:17 +0100

    [FRT-8] ignores folder structure

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -3,6 +3,7 @@ var runSequence = require('run-sequence');
 var changed = require('gulp-changed');
 var plumber = require('gulp-plumber');
 var to5 = require('gulp-babel');
+var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
 var paths = require('../paths');
 var compilerOptions = require('../babel-options');
@@ -15,25 +16,27 @@ var sass = require('gulp-sass');
 // the plumber() call prevents 'pipe breaking' caused
 // by errors from other gulp plugins
 // https://www.npmjs.com/package/gulp-plumber
-gulp.task('build-system', function() {
+gulp.task('build-system', function () {
   return gulp.src(paths.source)
     .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
     .pipe(changed(paths.output, {extension: '.js'}))
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(to5(assign({}, compilerOptions, {modules: 'system'})))
     .pipe(sourcemaps.write({includeContent: true}))
+    .pipe(rename({dirname: ''}))
     .pipe(gulp.dest(paths.output));
 });
 
 // copies changed html files to the output directory
-gulp.task('build-html', function() {
+gulp.task('build-html', function () {
   return gulp.src(paths.html)
     .pipe(changed(paths.output, {extension: '.html'}))
+    .pipe(rename({dirname: ''}))
     .pipe(gulp.dest(paths.output));
 });
 
 // copies changed css files to the output directory
-gulp.task('build-css', ['build-sass', 'copy-assets'], function() {
+gulp.task('build-css', ['build-sass', 'copy-assets'], function () {
   return gulp.src(paths.css)
     .pipe(changed(paths.output, {extension: '.css'}))
     .pipe(gulp.dest(paths.output))
@@ -41,23 +44,29 @@ gulp.task('build-css', ['build-sass', 'copy-assets'], function() {
 });
 
 // compile scss and copies to output directory
-gulp.task('copy-assets', function() {
+gulp.task('copy-assets', function () {
   gulp.src(paths.assets)
-      .pipe(gulp.dest(paths.output + 'assets/'));
+    .pipe(gulp.dest(paths.output + 'assets/'));
 });
 
 // compile scss and copies to output directory
-gulp.task('build-sass', function() {
+gulp.task('build-sass', function () {
   gulp.src(paths.scss)
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('styles'));
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('styles'));
 });
+
+//// all files in root of dist folder
+//gulp.task('build-dist', function() {
+//  gulp.src('src/app/**/*')
+//      .pipe(gulp.dest(paths.output));
+//});
 
 // this task calls the clean task (located
 // in ./clean.js), then runs the build-system
 // and build-html tasks in parallel
 // https://www.npmjs.com/package/gulp-run-sequence
-gulp.task('build', function(callback) {
+gulp.task('build', function (callback) {
   return runSequence(
     'clean',
     ['build-system', 'build-html', 'build-css'],
