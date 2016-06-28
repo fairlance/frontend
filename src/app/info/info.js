ebb9d8857457ec9088afe87002e37e354cb244f3
stefan <sopic.stefan@gmail.com>
2016-06-29 01:39:00 +0200

    client and profile page now different

--- src/app/info/info.js
+++ src/app/info/info.js
@@ -1,18 +1,20 @@
 import {inject} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
+import {User} from 'user';
 import {json} from 'aurelia-fetch-client';
 import 'fetch';
 
-@inject('AppHttpClient', Router)
+@inject('AppHttpClient', Router, User)
 export class Info {
   scrollTop = 0;
-  scrollLeft = 0;
+  skills = [];
+  screen = false;
 
-  constructor(http, router) {
+  constructor(http, router, user) {
     let first = this;
     this.http = http;
     this.router = router;
-    this.screen = false;
+    this.user = user.getCurrentUser().data;
 
     this.handleScrollEvent = e => {
       setTimeout(() => {first.screen = true}, 300);
@@ -28,30 +30,45 @@ export class Info {
     document.removeEventListener('scroll', this.handleScrollEvent);
   }
 
-  createUser = function () {
-    let user = {
-      firstName: this.firstName,
-      lastName: this.lastName,
-      email: this.email,
-      password: this.password
-    };
-    return json(user);
-  };
-
-  submit() {
+  addInfo = function () {
     var first = this;
-    console.log(first.createUser());
-    first.http.fetch('freelancer/new', {
-        method: 'post',
-        body: first.createUser()
+    first.http.fetch(first.user.type + '/' + first.user.id, {
+        method: 'update',
+        body: first.gatherInfo()
       })
       .then(function () {
         first.router.navigate('login');
       })
       .catch(function (error) {
         error.json().then(function (data) {
-
+          alert(data);
         });
       });
-  }
+  };
+
+  addSkill = function () {
+    this.skills.push(this.newSkill);
+    this.newSkill = '';
+  };
+
+  gatherInfo = function () {
+    let info;
+    if (this.user.type !== 'freelancer') {
+      info = {
+        skills: this.skills,
+        timezone: this.timezone,
+        available: this.available,
+        rateFrom: this.rateFrom,
+        rateTo: this.rateTo
+      };
+    } else {
+      info = {
+        timezone: this.timezone,
+        payment: this.payment,
+        industry: this.industry
+      };
+    }
+    return json(info);
+  };
+
 }
