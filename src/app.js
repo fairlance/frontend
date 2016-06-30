e0a2355d079e1fe4bfb8036a1bb12c11ce41894f
stefan <sopic.stefan@gmail.com>
2016-06-30 03:34:40 +0200

    jobs page markup done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ export class App {
       {route: ['', 'landing'], name: 'landing', moduleId: 'landing', title: 'Welcome'},
       {route: ['registration', 'registration'], name: 'registration', moduleId: 'registration', title: 'Register'},
       {route: ['info', 'info'], name: 'info', moduleId: 'info', title: 'Additional info'},
+      {route: ['jobs', 'jobs'], name: 'jobs', moduleId: 'jobs', title: 'Jobs'},
       {route: ['login', 'login'], name: 'login', moduleId: 'login', title: 'Login'},
       {route: ['client/:id', 'client/:id'], name: 'client', moduleId: 'client', title: 'Client'},
       {route: ['freelancer/:id', 'freelancer/:id'], name: 'freelancer', moduleId: 'freelancer', title: 'Freelancer'}
--- src/app/jobs/jobs.js
+++ src/app/jobs/jobs.js
@@ -7,6 +7,8 @@ import 'fetch';
 @inject('AppHttpClient', Router, User)
 export class Jobs {
 
+  jobs = [];
+
   constructor(http, router, user) {
 
     this.user = user.getCurrentUser().data;
@@ -16,72 +18,20 @@ export class Jobs {
 
   }
 
-  activate(params){
-    this.profileId = parseInt(params.id);
-    this.populateProfile();
+  getFilters() {
+    return;
   }
 
-  readProfileData(data) {
-    this.freelancer = data.data;
-  }
-
-  populateProfile() {
+  getJobs() {
     let first = this;
     first.http
-      .fetch('freelancer/' + this.profileId, {
+      .fetch('jobs/', {
         method: 'get',
+        body: this.getFilters(),
         headers: this.auth
       })
-      .then(function (response) {
-        response.json().then(function (data) {
-          first.readProfileData(data);
-        });
-      })
-      .catch(function (error) {
-        error.json().then(function (data) {
-          if (data.error === "Not logged in.") {
-            first.router.navigate('login');
-          }
-        });
-      });
-  }
-
-  showModal() {
-    this.dialog = document.querySelector('dialog');
-    if (!this.dialog.showModal) {
-      dialogPolyfill.registerDialog(this.dialog);
-    }
-    this.dialog.showModal();
-  }
-
-  hideModal() {
-    this.dialog.close();
-  }
-
-  prepareReference() {
-    let reference = {
-      freelancerId: this.user.id,
-      title: this.referenceTitle,
-      content: this.referenceContent,
-      media: {
-        image: this.imageUrl,
-        video: this.videoUrl
-      }
-    };
-    return json(reference);
-  }
-
-  addReference() {
-    let first = this;
-    first.http
-      .fetch('freelancer/' + first.user.id + '/reference', {
-        method: 'post',
-        body: this.prepareReference(),
-        headers: this.auth
-      })
-      .then(function () {
-        first.dialog.close();
-        first.populateProfile();
+      .then(function (data) {
+        first.jobs = data;
       })
       .catch(function (error) {
         console.log(error)
